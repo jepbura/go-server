7ba855da64c395d41021b524c3d42b804771d06c
jepbura <jepbura@gmail.com>
2023-12-24 14:48:03 +0330

    Commit Message: 	The commit message summarizes the changes made in the commit, which include refactoring the Resolver struct, importing the model package, implementing the CreateTodo functionality, and updating the Todos function. Commit Description: - Refactors the Resolver struct   - Adds a todos field of type model.Todo   - Updates import statement to include the model package - Implements CreateTodo functionality   - Generates a random number   - Creates a new Todo object with given input   - Appends the new Todo object to the Todos list - Removes previous panic error messages for CreateTodo and Todos functions.

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -6,19 +6,28 @@ package graph
 
 import (
 	"context"
+	"crypto/rand"
 	"fmt"
+	"math/big"
 
 	"github.com/jepbura/go-server/graph/model"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	randNumber, _ := rand.Int(rand.Reader, big.NewInt(100))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   fmt.Sprintf("T%d", randNumber),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return r.todos, nil
 }
 
 // Mutation returns MutationResolver implementation.
